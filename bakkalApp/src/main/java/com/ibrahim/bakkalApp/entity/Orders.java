b3ce12d8ef8366f88f9fa33e140b6dac70de47db
hisisman <hisisman3@gmail.com>
2025-02-02 15:07:45 +0300

    v1.0.1.2

--- bakkalApp/src/main/java/com/ibrahim/bakkalApp/entity/Orders.java
+++ bakkalApp/src/main/java/com/ibrahim/bakkalApp/entity/Orders.java
@@ -12,7 +12,7 @@ import java.util.Date;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Orders {
-//en son ki commit2
+//en son ki commit3
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
